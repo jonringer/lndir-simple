7032b758b6098fd49cf0de51db2091a95c40b393
Jonathan Ringer <jonringer117@gmail.com>
2024-09-01 12:55:55 -0700

    Init

---
+++ README.md
@@ -0,0 +1,13 @@
+# lndir (simple)
+
+This is similar to xorg's `lndir` utility, but optimized for usage within nixpkgs.
+Original motivation was to remove `xorg.lndir` from being in the `symlinkJoin`
+trivial builder of nixpkgs. Since `symlinkJoin` is used in the bootstrapping
+of stdenv, it creates a dependency on an additional 20-40 derivations
+
+Differences from xorg's lndir include:
+- Only dependencies are bash and gnu coreutils
+- Symlink locations are all resolved to absolute paths
+- `<from dir>` and `<destination dir>` must be defined
+- `<destination dir>` will be created if doesn't already exist
+- If `<from dir>` is a file, then just a symlink is created
---
+++ lndir.sh
@@ -0,0 +1,94 @@
+#!/usr/bin/env bash
+
+set -exo pipefail
+
+log() {
+  echo "$@" >&2
+}
+
+usage() {
+  log "lndir (simple) - Create shadow directories populated with symlinks to original source"
+  log ""
+  log "Usage:"
+  log "  lndir <from dir> <to dir>"
+}
+
+fromDir=$1
+toDir=$2
+
+# Remove all optional arguments, we assume silent, and don't respect
+# ignorelinks (deprecated) and -withrevinfo (not relevant for nix use case)
+while [ "$#" -gt 0 ]; do
+  i="$1"; shift 1
+  case "$i" in
+    -silent)
+      shift 1
+      ;;
+    -ignorelinks)
+      shift 1
+      ;;
+    -withrevinfo)
+      shift 1
+      ;;
+    --help)
+      usage
+      exit 1
+      ;;
+    *)
+      fromDir="$i"
+      break
+      ;;
+  esac
+done
+
+# fromDir was parsed of arg loop
+if [ "$#" -lt 1 ]; then
+  log "must pass <from dir> and <to dir> to lndir";
+  echo "$fromDir"
+  echo "$#"
+  echo "$@"
+  exit 1
+fi
+
+if [ "$#" -gt 1 ]; then
+  log "too many arguments passed to lndir";
+  exit 1
+fi
+
+toDir=$1
+
+# Do a DFS traversal, creating directories as they are encountered.
+# Files will be symlinked back to original source file
+shadow_copy_dir() {
+  local from="$1"
+  local to="$2"
+
+  for f in "${from}"/*; do
+    local baseName=
+    baseName="$(basename "$f")"
+
+    if [ -d "${from}/$baseName" ]; then
+      local newToDir="${to}/${baseName}"
+      local newFromDir="${from}/${baseName}"
+
+      mkdir -p "$newToDir"
+
+      shadow_copy_dir "$newFromDir" "$newToDir"
+    else
+      fullFromPath="$(realpath "${from}/${baseName}")"
+
+      ln -s "$fullFromPath" "${to}/$(basename "$f")"
+    fi
+  done
+}
+
+# Make the destination directory if it doesn't exist
+if [ ! -e "$toDir" ] && [ -d "$fromDir" ]; then
+  mkdir -p "$toDir"
+fi
+
+if [ ! -d "$fromDir" ]; then
+  ln -s "$(realpath "$fromDir")" "$(realpath "$toDir")"
+fi
+
+shadow_copy_dir "$(realpath "$fromDir")" "$(realpath "$toDir")"
